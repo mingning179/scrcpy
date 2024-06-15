268a719dc217103a1b8d311087c67f75d13f54f0
mingning179 <44414415+mingning179@users.noreply.github.com>
2024-06-15 11:45:04 +0800

    使用国内镜像

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,22 @@ group = "com.nothing"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    //阿里云
+    maven {
+        url = uri("https://maven.aliyun.com/repository/public")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/google")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/maven-central")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/jcenter")
+    }
     mavenCentral()
 }
 
